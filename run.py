26a680277951d62a6ea5bc55633f9bd3b9f99c03
sumanthratna <sumanthratna@gmail.com>
2019-11-09 17:55:53 -0500

    Add encryption

---
+++ run.py
@@ -0,0 +1,170 @@
+from tpm import TPM
+import numpy as np
+import time
+import sys
+import pydicom
+import getopt
+import matplotlib.pyplot as plt
+import pyAesCrypt
+
+
+def random(K, N, L):
+    '''random
+    return a random vector input for TPM
+    '''
+    return np.random.randint(-L, L + 1, [K, N])
+
+
+def sync_score(TPM1, TPM2, L):
+    '''sync_score
+    Synchronize the score of 2 tree parity machines
+    TPM1 - Tree Parity Machine 1
+    TPM2 - Tree Parity Machine 2
+    '''
+    return 1.0 - np.average(1.0 * np.abs(TPM1.W - TPM2.W) / (2 * L))
+
+
+def main(argv):
+    # default Tree Parity Machine parameters
+    K = 8
+    N = 12
+    L = 4
+    key_length = 128  # bits
+    iv_length = 128  # bits
+    update_rules = ['hebbian', 'anti_hebbian', 'random_walk']
+    update_rule = 'hebbian'
+    input_file = ''
+    output_file = 'out.enc'
+    has_input_file = False
+    try:
+        opts, args = getopt.getopt(argv, "hK:N:L:k:v:i:o:r:", [
+                                   "K=", "N=", "L=", "k=", "v=", "i=", "o=", "r="])
+    except getopt.GetoptError:
+        print('unknown options')
+        print('run.py -r hebbian -i <input file> -o <output file> -K <nb hidden neurons> -N <nb input neurons> -L <range of weight> -k <key length> -v <iv length>')
+        print('update rule : hebbian, anti_hebbian, random_walk')
+        print('key length options : 128, 192, 256')
+        print('iv length : [0:256] multiple of 4')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('run.py -r hebbian -i <input file> -o <output file> -K <nb hidden neurons> -N <nb input neurons> -L <range of weight> -k <key length> -v <iv length>')
+            print('update rule : hebbian, anti_hebbian, random_walk')
+            print('default values : K=8, N=12, L=4')
+            print('key length options : 128, 192, 256')
+            print('iv length : [0:256]')
+            sys.exit()
+        elif opt in ("-i", "--i"):
+            has_input_file = True
+            input_file = arg
+        elif opt in ("-o", "--o"):
+            output_file = int(arg)
+        elif opt in ("-K", "--K"):
+            K = int(arg)
+        elif opt in ("-N", "--N"):
+            N = int(arg)
+        elif opt in ("-L", "--L"):
+            L = int(arg)
+        elif opt in ("-v", "--v"):
+            iv_length = int(arg)
+        elif opt in ("-r", "--r"):
+            update_rule = str(arg)
+        elif opt in ("-k", "--k"):
+            if arg == "128" or arg == "192" or arg == "256":
+                key_length = int(arg)
+            else:
+                print('non available key options')
+                print('key length options : 128, 192, 256')
+                sys.exit()
+
+    # Create TPM for Alice, Bob and Eve. Eve eavesdrops communication of Alice and Bob
+    print("Creating machines : K=" + str(K) + ", N=" + str(N) + ", L="
+          + str(L) + ", k=" + str(key_length) + ", v=" + str(iv_length))
+    Alice = TPM(K, N, L)
+    Bob = TPM(K, N, L)
+    Eve = TPM(K, N, L)
+
+    # Synchronize weights
+    nb_updates = 0
+    nb_eve_updates = 0
+    start_time = time.time()  # Start time
+    sync_history = []  # plot purpose
+    sync_history_eve = []  # plot purpose
+    score = 0  # synchronisation score of Alice and Bob
+
+    while score < 100:
+
+        X = random(K, N, L)  # Create random vector [K, N]
+
+        # compute outputs of TPMs
+        tauA = Alice.get_output(X)
+        tauB = Bob.get_output(X)
+        tauE = Eve.get_output(X)
+
+        Alice.update(tauB, update_rule)
+        Bob.update(tauA, update_rule)
+
+        # Eve would update only if tauA = tauB = tauE
+        if tauA == tauB == tauE:
+            Eve.update(tauA, update_rule)
+            nb_eve_updates += 1
+
+        nb_updates += 1
+        # sync of Alice and Bob
+        # Calculate the synchronization of Alice and Bob
+        score = 100 * sync_score(Alice, Bob, L)
+        sync_history.append(score)  # plot purpose
+        # sync of Alice and Eve
+        # Calculate the synchronization of Alice and Eve
+        score_eve = 100 * sync_score(Alice, Eve, L)
+        sync_history_eve.append(score_eve)  # plot purpose
+
+        sys.stdout.write("\r" + "Synchronization = " + str(int(score)) + "%   /  Updates = "
+                         + str(nb_updates) + " / Eve's updates = " + str(nb_eve_updates))
+
+    end_time = time.time()
+    time_taken = end_time - start_time
+
+    # results
+    print("\nTime taken = " + str(time_taken) + " seconds.")
+    Alice_key, Alice_iv = Alice.makeKey(key_length, iv_length)
+    Bob_key, Bob_iv = Bob.makeKey(key_length, iv_length)
+    Eve_key, Eve_iv = Eve.makeKey(key_length, iv_length)
+    print("Alice's gen key = " + str(len(Alice_key))
+          + " key : " + Alice_key + " iv : " + Alice_iv)
+    print("Bob's gen key = " + str(len(Bob_key))
+          + " key : " + Bob_key + " iv : " + Bob_iv)
+    print("Eve's gen key = " + str(len(Eve_key))
+          + " key : " + Eve_key + " iv : " + Eve_iv)
+
+    if Alice_key == Bob_key and Alice_iv == Bob_iv:
+        try:
+            is_dicom = pydicom.dcmread(input_file) is not None
+        except(pydicom.errors.InvalidDicomError):
+            is_dicom = False
+        if has_input_file:
+            decrypt_file = "decipher.dcm" if is_dicom else "decipher.txt"
+            # cipher with AES
+            pyAesCrypt.encryptFile(
+                input_file, output_file, Alice_key, key_length)
+            # decipher with AES
+            pyAesCrypt.decryptFile(
+                output_file, decrypt_file, Alice_key, key_length)
+            print("encryption and decryption with aes"
+                  + str(key_length) + " done.")
+    else:
+        print("error, Alice and Bob have different key or iv : cipher impossible")
+
+    # Plot graph
+    plt.figure(1)
+    plt.title('Synchronisation')
+    plt.ylabel('sync %')
+    plt.xlabel('nb iterations')
+    sync_AB, = plt.plot(sync_history)
+    sync_Eve, = plt.plot(sync_history_eve)
+    plt.legend([sync_AB, sync_Eve], ["sync Alice Bob", "sync Alice Eve"])
+    plt.show()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ tpm.py
@@ -0,0 +1,104 @@
+import numpy as np
+import hashlib
+
+
+def theta(t1, t2):
+    return t1 == t2
+
+
+class TPM:
+    '''TPM
+    Tree Parity Machine is a special type of multi-layer feed-forward neural network.
+    K - number of hidden neurons
+    N - number of input neurons connected to each hidden neuron
+    L - range of each weight ({-L,..,0,..,+L })
+    W - weight matrix between input and hidden layers. Dimensions : [K, N]
+    tau - output score
+    '''
+
+    def __init__(self, K=8, N=12, L=4):
+        self.K = K
+        self.N = N
+        self.L = L
+        self.W = np.random.randint(-L, L + 1, [K, N])
+        self.tau = 0
+
+    def get_output(self, X):
+        '''
+        Returns a binary digit tau for a given random vecor.
+        X - Input random vector
+        '''
+        X = X.reshape([self.K, self.N])
+
+        sigma = np.sign(np.sum(X * self.W, axis=1))
+        tau = np.prod(sigma)
+
+        self.X = X
+        self.sigma = sigma
+        self.tau = tau
+
+        return tau
+
+    def hebbian(self, tau1, tau2):
+        '''
+        hebbian update rule
+        '''
+        for (i, j), _ in np.ndenumerate(self.W):
+            self.W[i, j] += self.X[i, j] * tau1 * \
+                theta(self.sigma[i], tau1) * theta(tau1, tau2)
+            self.W[i, j] = np.clip(self.W[i, j], -self.L, self.L)
+
+    def anti_hebbian(self, tau1, tau2):
+        '''
+        anti-hebbian update rule
+        '''
+        for (i, j), _ in np.ndenumerate(self.W):
+            self.W[i, j] -= self.X[i, j] * tau1 * \
+                theta(self.sigma[i], tau1) * theta(tau1, tau2)
+            self.W[i, j] = np.clip(self.W[i, j], -self.L, self.L)
+
+    def random_walk(self, tau1, tau2):
+        '''
+        random walk update rule
+        '''
+        for (i, j), _ in np.ndenumerate(self.W):
+            self.W[i, j] += self.X[i, j] * \
+                theta(self.sigma[i], tau1) * theta(tau1, tau2)
+            self.W[i, j] = np.clip(self.W[i, j], -self.L, self.L)
+
+    def update(self, tau2, update_rule='hebbian'):
+        '''
+        Updates the weights according to the specified update rule.
+        tau2 - Output bit from the other machine;
+        update_rule - The update rule : ['hebbian', 'anti_hebbian', random_walk']
+        '''
+        if self.tau == tau2:
+            if update_rule == 'hebbian':
+                self.hebbian(self.tau, tau2)
+            elif update_rule == 'anti_hebbian':
+                self.anti_hebbian(self.tau, tau2)
+            elif update_rule == 'random_walk':
+                self.random_walk(self.tau, tau2)
+            else:
+                raise Exception("Invalid update rule. Valid update rules are: "
+                                + "\'hebbian\', \'anti_hebbian\' and \'random_walk\'.")
+
+    # make key from weight matrix
+    def makeKey(self, key_length, iv_length):
+        '''
+        weight matrix to key and iv : use sha256 on concatenated weights
+        '''
+        key = ''
+        iv = ''
+        # generate key
+        for (i, j), _ in np.ndenumerate(self.W):
+            if i == j:
+                iv += str(self.W[i, j])
+            key += str(self.W[i, j])
+        # sha256 iv
+        hash_object_iv = hashlib.sha256(iv.encode('utf-8'))
+        hex_dig_iv = hash_object_iv.hexdigest()
+        # sha256 key
+        hash_object_key = hashlib.sha256(key.encode('utf-8'))
+        hex_dig_key = hash_object_key.hexdigest()
+        return (hex_dig_key[0:int(key_length / 4)], hex_dig_iv[0:int(iv_length / 4)])

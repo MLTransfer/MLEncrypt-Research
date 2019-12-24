a00ad819d75b12ca051a6283cc1d6de7971c540c
sumanthratna <sumanthratna@gmail.com>
2019-12-23 22:22:03 -0500

    Update tb script v3

--- tb.sh
+++ tb.sh
@@ -2,7 +2,7 @@
 if [ $1 == "tj" ]; then
   ssh -L 6006:127.0.0.1:6007 tj "cd MLEncrypt-Research; ./tb.sh snowy"
 elif [ $1 == "snowy" ]; then
-  ssh -L 6007:127.0.0.1:16006 snowy "cd MLEncrypt-Research; /home/2022sratna/.local/bin/pipenv shell; tensorboard --logdir logs/hparams/"
+  ssh -L 6007:127.0.0.1:6006 snowy "cd MLEncrypt-Research; /home/2022sratna/.local/bin/pipenv run tensorboard --logdir logs/hparams/"
 else
   echo "Invalid argument"
   exit 128

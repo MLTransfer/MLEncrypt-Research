bd3e4faff643e1c02f7172e2a23ab264c7088edd
Sumanth Ratna <sumanthratna@gmail.com>
2020-05-22 14:45:05 -0400

    Add tests Run with `poetry run python -m pytest tests`

---
+++ tests/test_single.py
@@ -0,0 +1,33 @@
+from click.testing import CliRunner
+from mlencrypt_research import cli  # noqa
+
+
+def test_single():
+    runner = CliRunner()
+    for ur in [
+        'random-same',
+        # 'random-different-A-B-E',
+        # 'random-different-A-B',
+        'hebbian',
+        'anti_hebbian',
+        'random_walk',
+    ]:
+        for attack in ['none', 'geometric', 'probabilistic']:
+            result = runner.invoke(
+                cli.cli,
+                ['single', '-ur', ur, '-a', attack],
+                catch_exceptions=False
+            )
+            assert result.exit_code == 0
+            assert not result.exception
+            assert result.exit_code == 0
+
+            with runner.isolated_filesystem():
+                result_tb = runner.invoke(
+                    cli.cli,
+                    ['single', '-ur', ur, '-a', attack, '-tb'],
+                    catch_exceptions=False
+                )
+                assert result_tb.exit_code == 0
+                assert not result_tb.exception
+                assert result_tb.exit_code == 0

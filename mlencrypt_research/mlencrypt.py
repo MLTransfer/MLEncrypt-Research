55ca752f5d40d25e39625207d0bf18cf98549ff7
Sumanth Ratna <sumanthratna@gmail.com>
2020-05-23 21:54:39 -0400

    Wrap contents of run in tf.experimental.async_scope

--- mlencrypt_research/mlencrypt.py
+++ mlencrypt_research/mlencrypt.py
@@ -139,24 +139,16 @@ def iterate(
 
     # TODO: use tf.cond
     if environ["MLENCRYPT_TB"] == 'TRUE':
-        try:
-            with tf.experimental.async_scope():
-                tb_summary('inputs', X)
-                K, N = Alice.K, Alice.N
-                hpaxis, ipaxis = tf.range(1, K + 1), tf.range(1, N + 1)
-                tb_heatmap('inputs', X, ipaxis, hpaxis)
-                tb_boxplot('inputs', X, hpaxis)
-        except tf.errors.OutOfRangeError:
-            tf.experimental.async_clear_error()
+        tb_summary('inputs', X)
+        K, N = Alice.K, Alice.N
+        hpaxis, ipaxis = tf.range(1, K + 1), tf.range(1, N + 1)
+        tb_heatmap('inputs', X, ipaxis, hpaxis)
+        tb_boxplot('inputs', X, hpaxis)
 
     # compute outputs of TPMs
-    try:
-        with tf.experimental.async_scope():
-            tauA = Alice.get_output(X)
-            tauB = Bob.get_output(X)
-            tauE = Eve.get_output(X)
-    except tf.errors.OutOfRangeError:
-        tf.experimental.async_clear_error()
+    tauA = Alice.get_output(X)
+    tauB = Bob.get_output(X)
+    tauE = Eve.get_output(X)
     updated_A_B = Alice.update(tauB, update_rule_A) \
         and Bob.update(tauA, update_rule_B)
     if updated_A_B:
@@ -188,27 +180,23 @@ def iterate(
     tf.cond(log_tb, true_fn=log_updates_E,
             false_fn=lambda: None, name='tb-updates-E')
 
-    try:
-        with tf.experimental.async_scope():
-            def compute_and_log_keys_and_ivs():
-                Alice_key, Alice_iv = Alice.compute_key(key_length, iv_length)
-                Bob_key, Bob_iv = Bob.compute_key(key_length, iv_length)
-                Eve_key, Eve_iv = Eve.compute_key(key_length, iv_length)
+    def compute_and_log_keys_and_ivs():
+        Alice_key, Alice_iv = Alice.compute_key(key_length, iv_length)
+        Bob_key, Bob_iv = Bob.compute_key(key_length, iv_length)
+        Eve_key, Eve_iv = Eve.compute_key(key_length, iv_length)
 
-            tf.cond(log_tb, true_fn=compute_and_log_keys_and_ivs,
-                    false_fn=lambda: None, name='tb-keys-ivs')
+    tf.cond(log_tb, true_fn=compute_and_log_keys_and_ivs,
+            false_fn=lambda: None, name='tb-keys-ivs')
 
-            score.assign(100. * sync_score(Alice, Bob), name='calc-sync-A-B')
-            score_eve.assign(100. * sync_score(Alice, Eve),
-                             name='calc-sync-A-E')
+    score.assign(100. * sync_score(Alice, Bob), name='calc-sync-A-B')
+    score_eve.assign(100. * sync_score(Alice, Eve),
+                     name='calc-sync-A-E')
 
-            def calc_and_log_sync_B_E():
-                sync_score(Bob, Eve)
-            # log adversary score for Bob's weights
-            tf.cond(log_tb, true_fn=calc_and_log_sync_B_E,
-                    false_fn=lambda: None, name='calc-sync-B-E')
-    except tf.errors.OutOfRangeError:
-        tf.experimental.async_clear_error()
+    def calc_and_log_sync_B_E():
+        sync_score(Bob, Eve)
+    # log adversary score for Bob's weights
+    tf.cond(log_tb, true_fn=calc_and_log_sync_B_E,
+            false_fn=lambda: None, name='calc-sync-B-E')
 
     tf.print(
         "Update rule = ", (update_rule_A, update_rule_B, update_rule_E), " / "
@@ -228,166 +216,177 @@ def run(
     initial_weights,
     key_length=256, iv_length=128
 ):
-    tf.print(
-        "\n\n\n",
-        "Creating machines: K=", K, ", N=", N, ", L=", L, ", ",
-        "update-rule=", update_rule, ", ",
-        "attack=", attack,
-        "\n",
-        sep='',
-        name='log-run-initialization'
-    )
-    Alice = TPM('Alice', K, N, L, initial_weights['Alice'])
+    with tf.experimental.async_scope():
 
-    Bob = TPM('Bob', K, N, L, initial_weights['Bob'])
-
-    # TODO: don't reimport entire file:
-    tpm_mod = import_module('mlencrypt_research.tpm')
-    if attack == 'probabilistic':
-        Eve_class_name = 'ProbabilisticTPM'
-    elif attack == 'geometric':
-        Eve_class_name = 'GeometricTPM'
-    elif attack == 'none':
-        Eve_class_name = 'TPM'
-    else:
-        # TODO: better message for ValueError
-        raise ValueError
-    Eve_class = getattr(tpm_mod, Eve_class_name)
-    Eve = Eve_class('Eve', K, N, L, initial_weights['Eve'])
-
-    score_dtype = tf.float32 if environ["MLENCRYPT_TB"] == 'TRUE' \
-        else tf.float16
-
-    try:
-        # synchronization score of Alice and Bob
-        score = tf.Variable(0.0, trainable=False,
-                            name='score-A-B', dtype=score_dtype)
-
-        # synchronization score of Alice and Eve
-        score_eve = tf.Variable(0.0, trainable=False,
-                                name='score-A-E', dtype=score_dtype)
-    except ValueError:
-        # tf.function-decorated function tried to create variables
-        # on non-first call.
-        score = 0.
-        score_eve = 0.
-
-    # https://www.tensorflow.org/tutorials/customization/performance#zero_iterations
-    with Alice.name_scope:
-        Alice.key = tf.Variable("", trainable=False, name='key')
-        Alice.iv = tf.Variable("", trainable=False, name='iv')
-    with Bob.name_scope:
-        Bob.key = tf.Variable("", trainable=False, name='key')
-        Bob.iv = tf.Variable("", trainable=False, name='iv')
-    with Eve.name_scope:
-        Eve.key = tf.Variable("", trainable=False, name='key')
-        Eve.iv = tf.Variable("", trainable=False, name='iv')
-
-    try:
-        nb_updates = tf.Variable(
-            0, name='updates-A-B', trainable=False, dtype=tf.int64)
-        nb_eve_updates = tf.Variable(
-            0, name='updates-E', trainable=False, dtype=tf.int64)
-    except ValueError:
-        # tf.function-decorated function tried to create variables
-        # on non-first call.
-        pass
-    tf.summary.experimental.set_step(0)
-
-    def train_step():
-        # Create random vector, X, with dimensions [K, N] and values {-1, 0, 1}
-        X = tf.Variable(
-            tf.random.uniform(
-                (K, N), minval=-1, maxval=1 + 1, dtype=tf.int32),
-            trainable=False,
-            name='input'
+        tf.print(
+            "\n\n\n",
+            "Creating machines: K=", K, ", N=", N, ", L=", L, ", ",
+            "update-rule=", update_rule, ", ",
+            "attack=", attack,
+            "\n",
+            sep='',
+            name='log-run-initialization'
         )
+        Alice = TPM('Alice', K, N, L, initial_weights['Alice'])
 
-        update_rules = tf.constant(['hebbian', 'anti_hebbian', 'random_walk'])
+        Bob = TPM('Bob', K, N, L, initial_weights['Bob'])
 
-        if update_rule == 'random-same':
-            # use tf.random so that the same update rule is used for each
-            # iteration across attacks
-
-            current_update_rule = tf.constant(select_random_from_list(
-                update_rules,
-                op_name='iteration-ur-A-B-E'
-            ))
-            update_rule_A = current_update_rule
-            update_rule_B = current_update_rule
-            update_rule_E = current_update_rule
-        elif update_rule.startswith('random-different'):
-            update_rule_A = tf.constant(select_random_from_list(
-                update_rules,
-                op_name='iteration-ur-A'
-            ))
-            update_rule_B = tf.constant(select_random_from_list(
-                update_rules,
-                op_name='iteration-ur-B'
-            ))
-            if update_rule == 'random-different-A-B-E':
-                update_rule_E = tf.constant(select_random_from_list(
-                    update_rules,
-                    op_name='iteration-ur-E'
-                ))
-            elif update_rule == 'random-different-A-B':
-                update_rule_E = update_rule_A
-            else:
-                raise ValueError
-        elif update_rule in update_rules:
-            current_update_rule = tf.constant(update_rule)
-            update_rule_A = current_update_rule
-            update_rule_B = current_update_rule
-            update_rule_E = current_update_rule
+        # TODO: don't reimport entire file:
+        tpm_mod = import_module('mlencrypt_research.tpm')
+        if attack == 'probabilistic':
+            Eve_class_name = 'ProbabilisticTPM'
+        elif attack == 'geometric':
+            Eve_class_name = 'GeometricTPM'
+        elif attack == 'none':
+            Eve_class_name = 'TPM'
         else:
-            # TODO: raise an appropriate error/exception
+            # TODO: better message for ValueError
+            raise ValueError
+        Eve_class = getattr(tpm_mod, Eve_class_name)
+        Eve = Eve_class('Eve', K, N, L, initial_weights['Eve'])
+
+        score_dtype = tf.float32 if environ["MLENCRYPT_TB"] == 'TRUE' \
+            else tf.float16
+
+        try:
+            # synchronization score of Alice and Bob
+            score = tf.Variable(0.0, trainable=False,
+                                name='score-A-B', dtype=score_dtype)
+
+            # synchronization score of Alice and Eve
+            score_eve = tf.Variable(0.0, trainable=False,
+                                    name='score-A-E', dtype=score_dtype)
+        except ValueError:
+            # tf.function-decorated function tried to create variables
+            # on non-first call.
+            score = 0.
+            score_eve = 0.
+
+        # https://www.tensorflow.org/tutorials/customization/performance#zero_iterations
+        with Alice.name_scope:
+            Alice.key = tf.Variable("", trainable=False, name='key')
+            Alice.iv = tf.Variable("", trainable=False, name='iv')
+        with Bob.name_scope:
+            Bob.key = tf.Variable("", trainable=False, name='key')
+            Bob.iv = tf.Variable("", trainable=False, name='iv')
+        with Eve.name_scope:
+            Eve.key = tf.Variable("", trainable=False, name='key')
+            Eve.iv = tf.Variable("", trainable=False, name='iv')
+
+        try:
+            nb_updates = tf.Variable(
+                0, name='updates-A-B', trainable=False, dtype=tf.int64)
+            nb_eve_updates = tf.Variable(
+                0, name='updates-E', trainable=False, dtype=tf.int64)
+        except ValueError:
+            # tf.function-decorated function tried to create variables
+            # on non-first call.
             pass
-        iterate(
-            X,
-            Alice, Bob, Eve,
-            update_rule_A, update_rule_B, update_rule_E,
-            nb_updates, nb_eve_updates,
-            score, score_eve,
-            key_length, iv_length
-        )
-        tf.summary.experimental.set_step(nb_updates)
+        tf.summary.experimental.set_step(0)
 
-    # instead of while, use for until L^4*K*N and break
-    weights_A_B_equal = tf.reduce_all(
-        tf.math.equal(Alice.w, Bob.w, name='weights-A-B-equal-elementwise'),
-        name='weights-A-B-equal'
-    )
-    start_time = perf_counter()
-    while score < 100. and not weights_A_B_equal:
-        train_step()
+        def train_step():
+            # Create random vector, X, with dimensions [K, N] and values
+            # bound by [-1, 1]
+            X = tf.Variable(
+                tf.random.uniform(
+                    (K, N), minval=-1, maxval=1 + 1, dtype=tf.int32),
+                trainable=False,
+                name='input'
+            )
 
-    end_time = perf_counter()
-    training_time = end_time - start_time
-    # loss = (tf.math.sigmoid(training_time) + score_eve / 100.) / 2.
-    loss = (
-        tf.math.log(tf.cast(training_time, score_dtype)) + score_eve / 100.
-    ) / 2.
-    key_length, iv_length = tf.constant(key_length), tf.constant(iv_length)
-    if tf.math.equal(environ["MLENCRYPT_TB"], 'TRUE', name='log-tb'):
-        # creates scatterplots (in scalars dashboard) of metric vs steps
-        tf.summary.scalar('training_time', training_time)
-        tf.summary.scalar('eve_score', score_eve)
-        tf.summary.scalar('loss', loss)
+            tpm_update_rules = tf.constant([
+                'hebbian',
+                'anti_hebbian',
+                'random_walk'
+            ])
 
-        tf.print(
-            "\n\n",
-            "Training time = ", training_time, " seconds.\n",
-            "Alice's key: ", Alice.key, " iv: ", Alice.iv, "\n",
-            "Bob's key: ", Bob.key, " iv: ", Bob.iv, "\n",
-            "Eve's key: ", Eve.key, " iv: ", Eve.iv,
-            sep='',
-            name='log-run-final'
+            if update_rule == 'random-same':
+                # use tf.random so that the same update rule is used for
+                # each iteration across attacks
+
+                current_update_rule = tf.constant(select_random_from_list(
+                    tpm_update_rules,
+                    op_name='iteration-ur-A-B-E'
+                ))
+                update_rule_A = current_update_rule
+                update_rule_B = current_update_rule
+                update_rule_E = current_update_rule
+            elif update_rule.startswith('random-different'):
+                update_rule_A = tf.constant(select_random_from_list(
+                    tpm_update_rules,
+                    op_name='iteration-ur-A'
+                ))
+                update_rule_B = tf.constant(select_random_from_list(
+                    tpm_update_rules,
+                    op_name='iteration-ur-B'
+                ))
+                if update_rule == 'random-different-A-B-E':
+                    update_rule_E = tf.constant(select_random_from_list(
+                        tpm_update_rules,
+                        op_name='iteration-ur-E'
+                    ))
+                elif update_rule == 'random-different-A-B':
+                    update_rule_E = update_rule_A
+                else:
+                    raise ValueError
+            elif update_rule in tpm_update_rules:
+                current_update_rule = tf.constant(update_rule)
+                update_rule_A = current_update_rule
+                update_rule_B = current_update_rule
+                update_rule_E = current_update_rule
+            else:
+                # TODO: raise an appropriate error/exception
+                pass
+            iterate(
+                X,
+                Alice, Bob, Eve,
+                update_rule_A, update_rule_B, update_rule_E,
+                nb_updates, nb_eve_updates,
+                score, score_eve,
+                key_length, iv_length
+            )
+            tf.summary.experimental.set_step(nb_updates)
+
+        # instead of while, use for until L^4*K*N and break
+        weights_A_B_equal = tf.reduce_all(
+            tf.math.equal(Alice.w, Bob.w,
+                          name='weights-A-B-equal-elementwise'),
+            name='weights-A-B-equal'
         )
-    else:
-        tf.print(
-            "\n\n",
-            "Training time = ", training_time, " seconds.\n",
-            sep='',
-            name='log-run-final'
-        )
+        start_time = perf_counter()
+        while score < 100. and not weights_A_B_equal:
+            train_step()
+
+        end_time = perf_counter()
+        training_time = end_time - start_time
+        # loss = (tf.math.sigmoid(training_time) + score_eve / 100.) / 2.
+        loss = tf.math.accumulate_n([
+            tf.math.log(tf.cast(training_time, score_dtype)),
+            score_eve / 100.
+        ], shape=[], tensor_dtype=tf.float16) / 2.
+        # ^scalars have shape []
+        key_length = tf.constant(key_length)
+        iv_length = tf.constant(iv_length)
+        if tf.math.equal(environ["MLENCRYPT_TB"], 'TRUE', name='log-tb'):
+            # create scatterplots (in scalars dashboard) of metric vs steps
+            tf.summary.scalar('training_time', training_time)
+            tf.summary.scalar('eve_score', score_eve)
+            tf.summary.scalar('loss', loss)
+
+            tf.print(
+                "\n\n",
+                "Training time = ", training_time, " seconds.\n",
+                "Alice's key: ", Alice.key, " iv: ", Alice.iv, "\n",
+                "Bob's key: ", Bob.key, " iv: ", Bob.iv, "\n",
+                "Eve's key: ", Eve.key, " iv: ", Eve.iv,
+                sep='',
+                name='log-run-final'
+            )
+        else:
+            tf.print(
+                "\n\n",
+                "Training time = ", training_time, " seconds.\n",
+                sep='',
+                name='log-run-final'
+            )
     return training_time, score_eve, loss

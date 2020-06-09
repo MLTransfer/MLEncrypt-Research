f7c5245ee82c81bc3294e9489d21cd4e3e60b80a
Sumanth Ratna <sumanthratna@gmail.com>
2020-06-08 21:30:53 -0400

    Use isolated filesystem for hparams command test

--- tests/test_hparams.py
+++ tests/test_hparams.py
@@ -1,15 +1,20 @@
 from click.testing import CliRunner
 from mlencrypt_research import cli
+from os import mkdir
 
 
 def test_hparams():
     runner = CliRunner(env={'WANDB_MODE': 'dryrun'})
-    # with runner.isolated_filesystem():
-    result = runner.invoke(
-        cli.cli,
-        ['hparams', 'nevergrad', '-n', '2'],
-        catch_exceptions=False
-    )
-    assert result.exit_code == 0
-    assert not result.exception
-    assert result.exit_code == 0
+    with runner.isolated_filesystem():
+        mkdir('./wandb')
+        with open('./wandb/settings', mode='w') as wandb_settings_file:
+            wandb_settings_file.write('[default]\n\n')
+
+        result = runner.invoke(
+            cli.cli,
+            ['hparams', 'nevergrad', '-n', '2'],
+            catch_exceptions=False
+        )
+        assert result.exit_code == 0
+        assert not result.exception
+        assert result.exit_code == 0
